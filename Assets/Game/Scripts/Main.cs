c3479a71f4d45b1de5c7468ea7ab6ccb73b745b4
Willian Gomes de Oliveira <Willian Gomes de Oliveira>
2016-10-18 23:38:50 -0200

    Testes xml

---
+++ Assets/Game/Scripts/Main.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+using UnityEngine;
+using System.Collections;
+
+public class Main : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		StartCoroutine(loadXml());
+	}
+	
+	IEnumerator loadXml()
+	{
+		var www = new WWW(Path.Combine(Application.dataPath, "Game/Data/monsters.xml"));
+		yield return www;
+
+		Debug.Log(www.text);
+		var monsterCollection = MonsterContainer.LoadFromText(www.text);
+
+		
+	}
+
+}
