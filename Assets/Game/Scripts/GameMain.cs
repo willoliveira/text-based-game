04cd66e2b6414dad0b3b6eb13639f10c178633d2
--global <willian.goliveira@outlook.com>
2016-11-17 21:21:10 -0200

    Programado a passagem de dialogos

--- Assets/Game/Scripts/GameMain.cs
+++ Assets/Game/Scripts/GameMain.cs
@@ -5,7 +5,12 @@ using System.Collections.Generic;
 
 public class GameMain : MonoBehaviour {
 
-	public Dictionary<string, Dialogue> DialogueList =  new Dictionary<string, Dialogue>();
+	public GameObject dialoguesContainer;
+	public GameObject dialoguePanel;
+	public GameObject dialogueOption;
+
+	private Dictionary<string, Dialogue> DialogueList = new Dictionary<string, Dialogue>();
+	private GameObject panelDialogue;
 
 	// Use this for initialization
 	void Start () {
@@ -46,6 +51,7 @@ public class GameMain : MonoBehaviour {
 		D.AddNode(DNode);
 
 		//definindo o nó inicial
+		//TODO: usar isso depois
 		D.actual_dialogue_id = "race_question";
 
 
@@ -55,42 +61,115 @@ public class GameMain : MonoBehaviour {
 		StartDialogue(D);
 	}
 
-	public GameObject dialoguesContainer;
-	public GameObject dialoguePanel;
-	public GameObject dialogueOption;
-
-	GameObject instance;
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="dialogue"></param>
 	void StartDialogue(Dialogue dialogue)
 	{
 		//colocando o dialogue panel na tela
-		instance = Instantiate(dialoguePanel, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
-		instance.transform.SetParent(dialoguesContainer.transform, false);
+		panelDialogue = Instantiate(dialoguePanel, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
+		panelDialogue.transform.SetParent(dialoguesContainer.transform, false);
 		//chama o proximo dialogo
 		NextDialgue(dialogue.actual_dialogue_id);
 	}
-
+	/// <summary>
+	/// 
+	/// todo: depois implementar um lance de limpar ou não o dialogo anterior
+	/// todo: fazer um lance que de pra voltar dialogos
+	/// </summary>
+	/// <param name="idDialogue"></param>
 	void NextDialgue(string idDialogue)
 	{
 		//dialoguePanel
 		//	Enunciation
 		//	OptionsContainer
+		//Valida a option
+		if (!ValidOptionDialogue(idDialogue)) return;
 		//set enunciado
-		instance.transform.Find("Enunciation").GetComponent<Text>().text = DialogueList["stat_dialogue"].dialogDictionary[idDialogue].enunciation;
+		panelDialogue.transform.Find("Enunciation").GetComponent<Text>().text = DialogueList["stat_dialogue"].dialogDictionary[idDialogue].enunciation;
 		//pega referencia do container
-		Transform optionsContainer = instance.transform.Find("OptionsContainer") as Transform;
+		Transform optionsContainer = panelDialogue.transform.Find("OptionsContainer") as Transform;
+		//TODO: Mudar isso aqui para reaproveitar os GO já criados...
+		//Limpas as alternativas
+		ClearOptionsDialogue(optionsContainer);
+		//Cria os options com base no conteudo dos options
 		foreach (DialogueOptions option in DialogueList["stat_dialogue"].dialogDictionary[idDialogue].options)
 		{
-			if (option.GetType() == typeof(DialogueOptionItem))
+			CreateOptionDialogue(option, optionsContainer);
+		}
+	}
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="idDialogue"></param>
+	/// <returns></returns>
+	bool ValidOptionDialogue(string idDialogue)
+	{
+		if (!DialogueList["stat_dialogue"].dialogDictionary.ContainsKey(idDialogue))
+		{
+			//Se for o fim
+			if (idDialogue == "{end}")
+				FinishDialogue();
+			else
 			{
-				//cache do option
-				DialogueOptionItem optionCache = (DialogueOptionItem) option;
-				//instanciando um option
-				GameObject instanceDialogueOption = Instantiate(dialogueOption, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
-				instanceDialogueOption.transform.SetParent(optionsContainer, false);
-				//colocando o texto da alternativa
-				instanceDialogueOption.transform.Find("OptionText").GetComponent<Text>().text = optionCache.text;
+				Debug.LogError("ID de dialogo não se encontra no dicionário");
+				FinishDialogue();
+			}
+			return false;
+		}
+		return true;
+	}
+	/// <summary>
+	/// 
+	/// </summary>
+	void FinishDialogue()
+	{
+		Debug.Log("Finish :)");
+		panelDialogue.gameObject.SetActive(false);
+	}
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="optionsContainer"></param>
+	void ClearOptionsDialogue(Transform optionsContainer)
+	{
+		//Mudar isso aqui para reaproveitar os GO já criados...
+		if (optionsContainer.childCount > 0)
+		{
+			for (int cont = 0, len = optionsContainer.childCount; cont < len; cont++)
+			{
+				Destroy(optionsContainer.GetChild(cont).gameObject);
 			}
 		}
-
 	}
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <param name="option"></param>
+	/// <param name="optionsContainer"></param>
+	void CreateOptionDialogue(DialogueOptions option, Transform optionsContainer)
+	{
+		if (option.GetType() == typeof(DialogueOptionItem))
+		{
+			//cache do option
+			DialogueOptionItem optionCache = (DialogueOptionItem)option;
+			//instanciando um option
+			GameObject instanceDialogueOption = Instantiate(dialogueOption, new Vector3(0, 0, 0), Quaternion.identity) as GameObject;
+			instanceDialogueOption.transform.SetParent(optionsContainer, false);
+			//colocando o texto da alternativa
+			instanceDialogueOption.transform.Find("OptionText").GetComponent<Text>().text = optionCache.text;
+
+			//Add a click
+			instanceDialogueOption.GetComponent<Button>().onClick.AddListener(delegate { onClickOption(option); });
+		}
+	}
+
+	void onClickOption(DialogueOptions option)
+	{
+		Debug.Log(option.nextNode);
+
+		NextDialgue(option.nextNode);
+	}
+
 }
